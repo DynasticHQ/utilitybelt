478eddee30df29596459c7a32ee70ebd32571fe5
Allen Sanabria <asanabria@linuxdynasty.org>
2016-02-22 13:32:18 -0500

    Pretty Printer for json

---
+++ utils_test.go
@@ -0,0 +1,17 @@
+package utils
+
+import "testing"
+
+func TestPrettyJson(t *testing.T) {
+	type multi [][]byte
+	testBytes := multi{
+		[]byte(`{"hello": "world"}`),
+		[]byte(`{"GoodBye": {"name": "Ninjas", "name": "Samurais"}}`),
+	}
+	for _, val := range testBytes {
+		transformed, err := PrettyJson(val)
+		if err != nil {
+			t.Error(err)
+		}
+	}
+}
