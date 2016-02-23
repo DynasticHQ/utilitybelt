4aa71a811681e120ac18ba9967498d9e4429157d
Allen Sanabria <asanabria@linuxdynasty.org>
2016-02-22 23:14:52 -0500

    Added test for failure

--- jsonext/jsonext_test.go
+++ jsonext/jsonext_test.go
@@ -1,17 +1,40 @@
 package jsonext
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestPrettyJson(t *testing.T) {
 	type multi [][]byte
 	testBytes := multi{
 		[]byte(`{"hello": "world"}`),
 		[]byte(`{"GoodBye": {"name": "Ninjas", "name": "Samurais"}}`),
+		[]byte(`"Foo"="Bar"`),
 	}
-	for _, val := range testBytes {
+	expectedOutput := []string{
+		`{
+  "hello": "world"
+}`,
+		`{
+  "GoodBye": {
+    "name": "Ninjas",
+    "name": "Samurais"
+  }
+}`, `"Foo"="Bar"`,
+	}
+	for i, val := range testBytes {
 		transformed, err := PrettyJson(val)
-		if err != nil {
+		switch {
+		case err != nil && i != 2:
 			t.Error(err)
+		case string(transformed) != expectedOutput[i] && i != 2:
+			t.Error("Outputs do not match")
+		case i == 2 && err == nil:
+			t.Error("err is suppose to be nil and not ", err)
+		case err == nil:
+			fmt.Printf("Output:\n%s\nExpected Output\n%s\n\n", transformed, expectedOutput[i])
+
 		}
 	}
 }
